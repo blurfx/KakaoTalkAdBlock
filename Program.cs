2bd6e879989821df70c3562571b8db3329f39d81
Changhui Lee <iam@xo.dev>
2019-05-22 04:52:15 +0900

    Revert "Revert "Faster Faster""

--- Program.cs
+++ Program.cs
@@ -168,9 +168,9 @@ namespace KakaoTalkAdBlock
                     {
                         isKakaotalkRunning = true;
                     }
-                    Thread.Sleep(1000);
+                    Thread.Sleep(16);
                 }
-                Thread.Sleep(1000);
+                Thread.Sleep(16);
             }
         }
 
@@ -239,9 +239,9 @@ namespace KakaoTalkAdBlock
                             SetWindowPos(childHwnd, IntPtr.Zero, 0, 0, width, height, SetWindowPosFlags.SWP_NOMOVE);
                         }
                     }
-                    Thread.Sleep(1000);
+                    Thread.Sleep(16);
                 }
-                Thread.Sleep(1000);
+                Thread.Sleep(16);
             }
         }
     }
