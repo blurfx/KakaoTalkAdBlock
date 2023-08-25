8fe8c3cdba570a87d0119b302ae78274e0d02110
blurfx <iam@xo.dev>
2023-08-26 00:59:16 +0900

    feat: support i386 arch

--- build.bat
+++ build.bat
@@ -6,4 +6,8 @@ go-winres simply -icon=icon.ico
 
 popd
 
-go build -o KakaoTalkAdBlock.exe -ldflags "-H windowsgui -s -w" .\cmd\main.go
+set GOARCH=amd64
+go build -o KakaoTalkAdBlock_amd64.exe -ldflags "-H windowsgui -s -w" .\cmd\main.go
+
+set GOARCH=386
+go build -o KakaoTalkAdBlock_i386.exe -ldflags "-H windowsgui -s -w" .\cmd\main.go
